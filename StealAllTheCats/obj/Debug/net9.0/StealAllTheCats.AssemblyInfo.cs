a8be3524fbc0553bc33bdd5d9211629293a29a1b
kMirzaev <kostas.mirzaev@gmail.com>
2025-06-01 11:48:28 +0300

    Move the README file to parent folder

--- StealAllTheCats/obj/Debug/net9.0/StealAllTheCats.AssemblyInfo.cs
+++ StealAllTheCats/obj/Debug/net9.0/StealAllTheCats.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StealAllTheCats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe09a3afee4fd8a14940ddbc9c4be8b57300d7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3ea40d658d8422a50796ef21360675c48f851a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StealAllTheCats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StealAllTheCats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
